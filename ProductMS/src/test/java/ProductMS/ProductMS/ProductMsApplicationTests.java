0cdb20d69f22803e66fe50393a694fde2697d4a3
Gowtham <gowtham.chilakaluri@cognizant.com>
2026-02-16 18:15:11 +0530

    Add files via upload

---
+++ ProductMS/src/test/java/ProductMS/ProductMS/ProductMsApplicationTests.java
@@ -0,0 +1,13 @@
+package ProductMS.ProductMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
